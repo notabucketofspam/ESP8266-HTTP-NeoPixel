40e24d1a6242540412096dcb747d29b9cdc21380
notabucketofspam <not.a.bucket.of.spam@gmail.com>
2020-07-15 11:39:39 -0400

    Update naming of included components

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-yes | rm -r include;mkdir include; cd include
+yes | rm -r component; mkdir component; cd component
 git clone https://github.com/adafruit/Adafruit_NeoPixel.git
-git clone https://github.com/notabucketofspam/ESP8266-general-include.git
-cp ESP8266-general-include/Kconfig.projbuild ../main/Kconfig.projbuild
+git clone https://github.com/notabucketofspam/ESP8266-general-component.git
 git clone https://github.com/notabucketofspam/ESP8266-HTML-GPIO.git
 cd ..
