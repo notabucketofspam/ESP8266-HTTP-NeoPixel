120a90c2335d56e945c9d6a0f0030e828456b455
notabucketofspam <not.a.bucket.of.spam@gmail.com>
2020-07-15 11:31:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# ESP8266-remote-Neopixel
+Web interface for controlling a strip of Neopixel Lights.
+
+---
+
+Example use case: Wemos D1 Mini with relay shield to control power.
---
+++ main/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "Adafruit_NeoPixel/Adafruit_NeoPixel.h"
+#include "html_gpio_core.h"
+
+#ifdef __cplusplus
+extern "C" { void app_main(void); }
+#endif
+
+void app_main(void) {
+    
+}
