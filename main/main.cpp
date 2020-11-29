e585fe32e46c089d115426bc552ce28c6c705f34
notabucketofspam <not.a.bucket.of.spam@gmail.com>
2020-11-28 19:20:04 -0500

    Back to square one

--- main/main.cpp
+++ main/main.cpp
@@ -2,15 +2,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "Adafruit_NeoPixel.h"
-#include "html_gpio_core.h"
 
 #ifdef __cplusplus
 extern "C" { void app_main(void); }
 #endif
 
 void app_main(void) {
-  ESP_LOGI(TAG, "app_main init");
-  core_config_t core_config = CORE_DEFAULT_CONFIG();
-  ESP_ERROR_CHECK(setup_core(&core_config));
-  ESP_LOGI(TAG, "app_main OK");
+  
 }
